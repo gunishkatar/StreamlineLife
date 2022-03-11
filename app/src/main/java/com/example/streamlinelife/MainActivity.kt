b14c734e06b37cf7cf094be24c60a7bf75b52629
pranavmahindru <pranavmahindru16@gmail.com>
2022-03-10 21:37:39 -0400

    almost done with home page

--- app/src/main/java/com/example/streamlinelife/MainActivity.kt
+++ app/src/main/java/com/example/streamlinelife/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.streamlinelife
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //add fragment as a home page
+        supportFragmentManager.commit {
+            setReorderingAllowed(true);
+            add<HomePage>(R.id.homePage)
+        }
     }
 }
