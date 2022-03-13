bf3374de77a5439ecaccb9d4448be1b009a3b59b
Gunish Katar <gn894984@dal.ca>
2022-03-13 15:04:28 -0300

    Created supported constants and Overrode the onCreate function

--- app/src/main/java/com/example/streamlinelife/DBSupport.kt
+++ app/src/main/java/com/example/streamlinelife/DBSupport.kt
@@ -12,10 +12,36 @@ class DBSupport(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, nul
         private const val DATABASE_NAME = "STREAMLINE_LIFE"
         private const val DATABASE_VERSION = 1
 
+        // Reminder table constants
+        private const val REMINDER_TABLE = "Reminders"
+        private const val ID_COL = "id";
+        private const val TITLE_COL = "title";
+        private const val DESCRIPTION_COL = "description";
+        private const val DATETIME_COL = "datetime";
+        private const val LOCATION_COL = "location";
+        private const val IMPORTANCE_COL = "importance";
+        private const val LIST_COL = "list_number";
+        private const val createRemindersTable = "CREATE TABLE $REMINDER_TABLE (" +
+                "$ID_COL INTEGER PRIMARY KEY," +
+                "$TITLE_COL TEXT," +
+                "$DESCRIPTION_COL TEXT," +
+                "$DATETIME_COL DATETIME," +
+                "$LOCATION_COL TEXT," +
+                "$IMPORTANCE_COL INTEGER," +
+                "$LIST_COL TEXT)"
+
+        // Group table constant
+        private const val GROUP_TABLE = "Groups"
+        private const val NAME_COL = "name";
+        private const val createGroupsTable = "CREATE TABLE ${GROUP_TABLE}_TABLE (" +
+                "$ID_COL INTEGER PRIMARY KEY," +
+                "$NAME_COL TEXT)"
+
     }
 
     override fun onCreate(db: SQLiteDatabase) {
-        TODO("Not yet implemented")
+        db.execSQL(createRemindersTable);
+        db.execSQL(createGroupsTable)
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVer: Int, newVer: Int) {
