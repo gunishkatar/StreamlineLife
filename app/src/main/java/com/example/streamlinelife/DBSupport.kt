a544f142022dd6a7931f50aae1377c488cf1e4a7
Gunish Katar <gn894984@dal.ca>
2022-03-14 20:34:27 -0300

    Added getAllReminders, getNumberofReminders, and updateNumberofRemindersInGroup

--- app/src/main/java/com/example/streamlinelife/DBSupport.kt
+++ app/src/main/java/com/example/streamlinelife/DBSupport.kt
@@ -38,7 +38,7 @@ class DBSupport(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, nul
         private const val REPEAT_COL = "repeat"
         private const val GROUP_COL = "list_number"
         private const val createRemindersTable = "CREATE TABLE $REMINDER_TABLE (" +
-                "$ID_COL INTEGER PRIMARY KEY," +
+                "$ID_COL INTEGER PRIMARY KEY AUTOINCREMENT," +
                 "$TITLE_COL TEXT," +
                 "$DESCRIPTION_COL TEXT," +
                 "$DATETIME_COL TEXT," +
@@ -50,9 +50,11 @@ class DBSupport(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, nul
         // Group table constant
         private const val GROUP_TABLE = "Groups"
         private const val NAME_COL = "name"
+        private const val NUMBER_OF_REMINDERS = "numberofreminders"
         private const val createGroupsTable = "CREATE TABLE ${GROUP_TABLE}_TABLE (" +
-                "$ID_COL INTEGER PRIMARY KEY," +
-                "$NAME_COL TEXT)"
+                "$ID_COL INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "$NAME_COL TEXT" +
+                "$NUMBER_OF_REMINDERS INTEGER)"
 
     }
 
@@ -64,6 +66,11 @@ class DBSupport(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, nul
     override fun onUpgrade(db: SQLiteDatabase, oldVer: Int, newVer: Int) {
         TODO("Not yet implemented")
     }
+
+//    override fun onDestroy() {
+//        this.close()
+//        super.onDestroy()
+//    }
 
     fun addReminder(title: String, desc: String, datetime: String, loc: String, imp: Int, repeat: Boolean, group: String): Boolean{
         val data = ContentValues()
@@ -89,10 +96,11 @@ class DBSupport(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, nul
         return true
     }
 
-    fun addGroup(name: String): Boolean{
+    fun addGroup(name: String, number_of_reminders: Int): Boolean{
         val data = ContentValues()
 
         data.put(NAME_COL, name)
+        data.put(NUMBER_OF_REMINDERS, number_of_reminders)
 
         val db = this.writableDatabase
 
@@ -105,6 +113,58 @@ class DBSupport(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, nul
         }
 
         return true
+    }
+
+    fun getAllReminders() {
+
+        val db = this.writableDatabase
+        val cursor = db.query(REMINDER_TABLE, null,null,null,null,null,null,null)
+
+        val itemIds = mutableListOf<Long>()
+        with(cursor) {
+            while (moveToNext()) {
+                val itemId = getLong(getColumnIndexOrThrow(ID_COL))
+                itemIds.add(itemId)
+            }
+        }
+        cursor.close()
+    }
+
+    fun getNumberofRemindersInGroup(name: String): Long{
+        val db = this.writableDatabase
+
+        val columns = arrayOf(NUMBER_OF_REMINDERS)
+
+        val selection = "${NAME_COL} = ?"
+        val selectionValue = arrayOf(name)
+
+        val cursor = db.query(GROUP_TABLE, columns,selection,selectionValue,null,null,null,null)
+
+        cursor.moveToNext()
+        val numberOfReminders = cursor.getLong(cursor.getColumnIndexOrThrow(NUMBER_OF_REMINDERS))
+
+        return numberOfReminders
+
+    }
+
+    fun updateNumberofRemindersInGroup(name: String, number_of_reminders: Int): Boolean{
+        val db = this.writableDatabase
+
+        val valueToUpdate = ContentValues().apply {
+            put(NUMBER_OF_REMINDERS, number_of_reminders)
+        }
+
+        val selection = "${NAME_COL} = ?"
+        val selectionValue = arrayOf(name)
+
+        val count = db.update(GROUP_TABLE, valueToUpdate,selection,selectionValue)
+
+        if(count > 1){
+            return false
+        }
+
+        return true
+
     }
 
 
